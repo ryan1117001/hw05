4acbfb8132634e423651a51f5e6405bc2dbd473a
kofi1997 <kdinizul@syr.edu>
2017-03-04 14:47:13 -0500

    initilization of c file

--- hw05.c
+++ hw05.c
@@ -11,6 +11,32 @@
 #define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
 int main(int argc, char* argv[]){
+  	bool opt_d, opt_h=false;
+  	char* backLocation="~/backups/";
+  	int opt;
+  	/*
+  		getops loop
+  		*/
+  	char buffer[EVENT_BUF_LEN];
+  	int x, i = 0;
+	char* p;
+	struct inotify_event* event;
+  	int fd = inotify_init();
+  	if ( fd < 0 ) {
+    printf("inotify init failed\n");
+    exit(EXIT_FAILURE);
+    /*
+    fd inisilization
+    add watches
+    */
+      	while(1){
+  		x=read(fd, buffer, EVENT_BUF_LEN);
+  		if ( x < 0 ) {
+    		printf("read failed\n");
 
-
+  		}
+  		/*
+  		for loop for event handling
+  		*/
+  	}
 }
